0bbb060983428153617c1fc55db97f92f2db2139
supermoon <supermoon@nexon.co.kr>
2019-06-27 12:49:18 +0900

    test hello controller append

--- src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
+++ src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -4,10 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @EnableConfigServer
 public class ConfigServerApplication extends SpringBootServletInitializer {
+
+	@RequestMapping(path = "/hello")
+	public String hello() {
+		return "hello";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
