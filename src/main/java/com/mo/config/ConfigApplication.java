bd8326b34451465b4f3d839f0bd1107abc75d68e
supermoon <supermoon@nexon.co.kr>
2019-07-17 17:49:21 +0900

    eureka 연동작업

--- src/main/java/com/mo/config/ConfigApplication.java
+++ src/main/java/com/mo/config/ConfigApplication.java
@@ -1,17 +1,16 @@
-package com.thoughtmechanix.confsvr;
+package com.mo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableConfigServer
-public class ConfigServerApplication extends SpringBootServletInitializer {
+public class ConfigApplication extends SpringBootServletInitializer {
 
 	@RequestMapping(path = "/hello")
 	public String hello() {
@@ -19,6 +18,6 @@ public class ConfigServerApplication extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ConfigApplication.class, args);
 	}
 }
