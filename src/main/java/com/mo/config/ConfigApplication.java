a91b88ab0c5eaa6f5a40418c308e663703a28cdd
supermoon <supermoon@nexon.co.kr>
2019-07-17 18:16:10 +0900

    check-logic

--- src/main/java/com/mo/config/ConfigApplication.java
+++ src/main/java/com/mo/config/ConfigApplication.java
@@ -1,5 +1,6 @@
 package com.mo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,10 +8,28 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @RestController
 @EnableConfigServer
 public class ConfigApplication extends SpringBootServletInitializer {
+
+	@Value("${spring.profiles}")
+	String profiles;
+
+	@Value("${server.port}")
+	String port;
+
+	@Value("${eureka.instance.hostname}")
+	String discovery;
+
+	@PostConstruct
+	public void init() {
+		System.out.println("profiles : " + profiles);
+		System.out.println("port : " + port);
+		System.out.println("discovery : " + discovery);
+	}
 
 	@RequestMapping(path = "/hello")
 	public String hello() {
